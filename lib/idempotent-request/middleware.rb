3c827ff5eca0a749e20f0a9a1066c3905486848e
Chong Xian <chongxian@easystore.co>
2025-03-24 15:27:19 +0800

    fix: update middleware - support idempotent.yml file - handle failsafe for storage class

--- lib/idempotent-request/middleware.rb
+++ lib/idempotent-request/middleware.rb
@@ -2,10 +2,11 @@ module IdempotentRequest
   class Middleware
     def initialize(app, config = {})
       @app = app
-      @config = config
-      @policy = config.fetch(:policy)
+      @config = config.merge(load_config())
+      @policy = @config.fetch(:policy)
       @notifier = ActiveSupport::Notifications if defined?(ActiveSupport::Notifications)
-      @conflict_response_status = config.fetch(:conflict_response_status, 429)
+      @concurrent_response_status = @config.fetch(:concurrent_response_status, 429)
+      @replayed_response_header = @config.fetch(:replayed_response_header, 'Idempotency-Replayed')
     end
 
     def call(env)
@@ -15,9 +16,8 @@ module IdempotentRequest
 
     def process(env)
       set_request(env)
-      request.env['idempotent.request'] = {}
       return app.call(request.env) unless process?
-      request.env['idempotent.request']['key'] = request.key
+      request.env['idempotent.request'] = { key: request.key }
       response = read_idempotent_request || write_idempotent_request || concurrent_request_response
       instrument(request)
       response
@@ -26,29 +26,55 @@ module IdempotentRequest
     private
 
     def storage
-      @storage ||= RequestManager.new(request, config)
+      @storage ||= RequestManager.new(request, config.merge(expire_time: expire_time_for_route))
+    end
+
+    def expire_time_for_route
+      policy_instance = policy.new(request, config)
+      policy_instance.respond_to?(:expire_time_for_route) ? policy_instance.expire_time_for_route : nil
     end
 
     def read_idempotent_request
-      request.env['idempotent.request']['read'] = storage.read
+      result = storage.read rescue nil
+      return if result.blank?
+
+      status, headers, response = result
+      headers.merge!(@replayed_response_header => true)
+      request.env['idempotent.request']['read'] = [status, headers, response]
     end
 
     def write_idempotent_request
-      return unless storage.lock
       begin
-        result = app.call(request.env)
-        request.env['idempotent.request']['write'] = result
-        storage.write(*result)
-      ensure
-        request.env['idempotent.request']['unlocked'] = storage.unlock
-        result
+        return unless storage.lock
+      rescue
+        request.env['idempotent.request']['error'] = 'Failed to lock the key'
       end
+
+      result = app.call(request.env)
+
+      begin
+        storage.write(*result)
+        request.env['idempotent.request']['write'] = result
+      rescue
+      end
+
+      request.env['idempotent.request']['unlocked'] = storage.unlock rescue nil
+
+      result
     end
 
     def concurrent_request_response
-      status = @conflict_response_status
+      status = @concurrent_response_status
       headers = { 'Content-Type' => 'application/json' }
-      body = [ Oj.dump('error' => 'Concurrent requests detected') ]
+      body = [
+        {
+          error: {
+            type: "TooManyRequests",
+            message: "Concurrent requests detected",
+            code: "too_many_requests"
+          }
+        }.to_json
+      ]
       request.env['idempotent.request']['concurrent_request_response'] = true
       Rack::Response.new(body, status, headers).finish
     end
@@ -61,7 +87,7 @@ module IdempotentRequest
 
     def should_be_idempotent?
       return false unless policy
-      policy.new(request).should?
+      policy.new(request, config).should?
     end
 
     def instrument(request)
@@ -71,6 +97,16 @@ module IdempotentRequest
     def set_request(env)
       @env = env
       @request ||= Request.new(env, config)
+    end
+
+    def load_config(config_file = Rails.root.join('config', 'idempotent.yml'))
+      return {} unless File.exist?(config_file)
+
+      config = YAML.load_file(config_file).deep_symbolize_keys
+      env = Rails.env.to_s.to_sym
+
+      # Use environment specific settings if available, otherwise use default
+      config[env] || config[:default] || {}
     end
   end
 end
